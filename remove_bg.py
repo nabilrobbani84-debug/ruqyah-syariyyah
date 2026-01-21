cc69c8c0f61fe715270a4b3b3d3a48c3c4e61d01
nabilrobbani84 <nabilrobbani6@gmail.com>
2026-01-21 20:50:40 +0700

    Add new transparent logo and update navbar/favicon

---
+++ remove_bg.py
@@ -0,0 +1,24 @@
+from PIL import Image
+
+def remove_background(input_path, output_path):
+    img = Image.open(input_path)
+    img = img.convert("RGBA")
+    datas = img.getdata()
+
+    newData = []
+    for item in datas:
+        # Change all white (also shades of whites)
+        # to transparent
+        if item[0] > 240 and item[1] > 240 and item[2] > 240:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(item)
+
+    img.putdata(newData)
+    img.save(output_path, "PNG")
+    print(f"Saved transparent image to {output_path}")
+
+try:
+    remove_background('static/images/logo_icon.png', 'static/images/logo_transparent.png')
+except Exception as e:
+    print(f"Error: {e}")
