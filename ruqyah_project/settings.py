d0f6f57f9c920e5f9e4cc434a04f928fcbc15100
nabilrobbani84 <nabilrobbani84@gmail.com>
2026-02-16 10:40:23 +0700

    Add Google AdSense script and update settings

--- ruqyah_project/settings.py
+++ ruqyah_project/settings.py
@@ -135,7 +135,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
